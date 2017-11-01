fd4c4b399519721527f75c3adff1e0307d995667
Marcus Ahnve <marcus@ahnve.com>
2017-11-01 01:09:50 +0100

    Better database

--- src/cljs/martian_dice/db.cljs
+++ src/cljs/martian_dice/db.cljs
@@ -1,4 +1,13 @@
 (ns martian-dice.db)
 
 (def default-db
-  {:name "re-frame"})
+  {:name "martian-dice"
+   :unused-dice 14
+   :selected-dice {:cow 0
+                   :human 0
+                   :chicken 0
+                   :blast 0
+                   :spaceship 0}
+
+   :players [{:name "hej"
+              :points 3}]})
