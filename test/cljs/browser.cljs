24b58f130fb1ff661e9bfe36914e9b5561815a54
Marcus Ahnve <marcus@ahnve.com>
2017-11-02 19:00:02 +0100

    tests

---
+++ test/cljs/browser.cljs
@@ -0,0 +1,4 @@
+(ns cljs.browser
+  (:require [martian-dice.game-test]))
+
+
---
+++ test/cljs/martian_dice/game_test.cljs
@@ -0,0 +1,7 @@
+(ns martian-dice.game-test
+  (:require [martian-dice.game :as game]
+            [cljs.test :refer-macros [deftest is testing run-tests]]))
+
+
+
+(run-tests)
